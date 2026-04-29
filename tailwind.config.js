860b1037d2386c2adbb65a0a60c3f84ab0468dd2
Anvils <lizulu01@gmail.com>
2026-04-29 05:26:00 +0000

    added tailwind color palette

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          warm4:"310906",
+          warm3:"633533",
+          warm2:"90605e",
+          warm1:"e9b6b4",
+          neutral1:"f3efe0",
+          cool1:"c1d3c8",
+          cool2:"8fb7b0",
+          cool3:"597380",
+          cool4:"3e5168",
+          cool5:"222e50"
+        },
+      }
+    }
+  },
+  plugins: [],
+}
